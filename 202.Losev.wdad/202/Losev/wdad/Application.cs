b73301e27ba61fc806c6c3804f18d49d78d32ae4
Karma123457 <svaz2018note4x@gmail.com>
2025-04-12 20:32:29 +0300

    ееее

---
+++ 202.Losev.wdad/202/Losev/wdad/Application.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starting_monkey_to_human_path._202.Losev.wdad._202.Losev.wdad
+{
+    internal class Application
+    {
+        public string Name { get; set; }
+    }
+}
